f8e2865b03ecd5f093c741dd702bed3063d7f214
Marc Leeman <marc.leeman@gmail.com>
2025-01-07 14:53:35 +0100

    correct tlv_set_git_vars

--- pre_commit_hooks/tlv_set_git_vars.py
+++ pre_commit_hooks/tlv_set_git_vars.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from __future__ import annotations
 
 import os
@@ -6,10 +8,11 @@ from collections.abc import Sequence
 
 
 def main(argv: Sequence[str] | None = None) -> int:
-    os.environ[ "GIT_AUTHOR_NAME" ] = "Marc Leeman"
-    os.environ[ "GIT_AUTHOR_EMAIL=" ] = "m.leeman@televic.com"
+    os.environ["GIT_AUTHOR_NAME"] = "Marc Leeman"
+    os.environ["GIT_AUTHOR_EMAIL"] = "m.leeman@televic.com"
 
     return 0
+
 
 if __name__ == '__main__':
     raise SystemExit(main())
