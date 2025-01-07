56a8cb1c7281c12829767ca230bdf9438be57ed4
Marc Leeman <marc.leeman@gmail.com>
2025-01-07 15:22:45 +0100

    add tlv-deb-copyright-year

---
+++ pre_commit_hooks/tlv_deb_copyright_year.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+import os
+import re
+import argparse
+from datetime import datetime
+from collections.abc import Sequence
+
+
+def check_debian_copyright(filenames: Sequence[str]) -> int:
+    retv = 0
+
+    if os.path.exists("debian/copyright"):
+        with open("debian/copyright", "r") as cp_file:
+            current_year = datetime.now().year
+            txt = cp_file.read()
+            if not re.findall(f"Copyright.*{current_year}", txt):
+                print(
+                    f"debian/copyright should include \"{current_year}\"")
+                retv = 1
+
+    return retv
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'filenames', nargs='*',
+        help='Filenames pre-commit believes are changed.',
+    )
+
+    args = parser.parse_args(argv)
+
+    return check_debian_copyright(args.filenames)
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
